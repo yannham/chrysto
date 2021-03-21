a7e38b5c2574afbb6b4538e53196537e3c3333b1
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-03-21 20:55:34 +0100

    Nix config

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      SYSTEMS = ["x86_64-linux"];
+
+      inherit (nixpkgs.lib) genAttrs substring;
+
+      version = "${substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
+
+      forAllSystems = f: genAttrs SYSTEMS (system: f system);
+
+      buildChrysto = { system, isShell ? false }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+        in pkgs.stdenv.mkDerivation {
+          name = "chrysto-${version}";
+
+          buildInputs = [
+            pkgs.bs-platform
+            pkgs.nodePackages.vue-cli ];
+
+          src = if isShell then null else self;
+
+          buildPhase = "bsb -make-wolrd && vue-cli-service build";
+          doCheck = false;
+        };
+      
+    in rec {
+      defaultPackage = forAllSystems (system: packages."${system}".build);
+      devShell = forAllSystems (system: buildChrysto { inherit system; isShell =
+        true; });
+      packages = forAllSystems (system: {
+        build = buildChrysto { inherit system; };
+      });
+    };
+}
